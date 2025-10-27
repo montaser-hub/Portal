b730e72dbb57f28fb75b29fbadf99097a8969082
Montaser Ismail <montaserismail20@gmail.com>
2025-10-27 17:45:38 +0300

    update

---
+++ apps/api/src/models/userModel.js
@@ -0,0 +1,107 @@
+import crypto from 'crypto';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  nickname: {
+    type: String,
+    minlength: 3,
+    maxlength: 10,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'manager'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false, // hide the password when returning the user document
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  //hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  //this point to cuerrent query
+  this.find({ active: { $ne: false } });
+  next();
+});
+//function to check if the inserted password is equle to the current password by using instance methods
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+    return JWTTimestamp < changedTimestamp; //True means changed(day or the time at which token is issued is greater than the change timestamp)
+  }
+  //False means Not changed(day or the time at which token is issued is less than the change timestamp)
+  return false;
+};
+
+userSchema.methods.changedPasswordRestToken = function () {
+  //generate a random token
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
