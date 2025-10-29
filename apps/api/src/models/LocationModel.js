49395daae3bbd0370c22684351e2243843b4ccec
montaser-hub <60078290+montaser-hub@users.noreply.github.com>
2025-10-30 00:15:43 +0300

    Update LocationModel.js

--- apps/api/src/models/LocationModel.js
+++ apps/api/src/models/LocationModel.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 const locationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  postalCode: { type: String },
-  address:[{
-    street: { type: String },
-    city: { type: String },
-    state: { type: String },
-    country: { type: String },
-    postalCode: { type: String }
-  }]
+  name: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  state: {
+    type: String 
+  },
+  country: {
+    type: String
+  },
+  postalCode: {
+    type: String
+  }
 }, { timestamps: true });
+
+locationSchema.index({ state: 1, street: 1 }, { unique: true });
 
 const Location = mongoose.model('Location', locationSchema);
 
