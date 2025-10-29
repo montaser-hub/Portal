fc6f002d2f28cc6fd833db838d53385dcbbb665a
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-10-30 00:11:48 +0300

    add Level&Position

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -2,13 +2,12 @@ import express from 'express';
 import userRouter from './routes/userRoutes.js'
 import shiftRouter from './routes/shiftRoutes.js'
 import scheduleRouter from './routes/scheduleRoutes.js'
-
+import positionRouter from './routes/positionRoutes.js';
+import levelRouter from "./routes/levelRouter.js";
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // parse URL-encoded bodies
-console.log(process.env.NODE_ENV);
 
 app.use((req, res, next) => {
   console.log('Hello from the MIDDLEWARE ✳️:');
@@ -17,8 +16,12 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
+
 app.use( '/api/v1/shifts', shiftRouter);
 app.use( '/api/v1/schedules', scheduleRouter);
+
+app.use('/api/v1/positions', positionRouter);
+app.use('/api/v1/levels', levelRouter);
 
 
 app.all('*', (req, res, next) => {
