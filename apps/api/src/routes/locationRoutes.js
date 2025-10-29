5f72851611b1736d3bb8734c6ee9e39d16b69ee2
Tarek Hamdy Arafa <tarekhamdy7469@gmail.com>
2025-10-29 20:51:04 +0300

    Update locationRoutes.js

--- apps/api/src/routes/locationRoutes.js
+++ apps/api/src/routes/locationRoutes.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import * as locationController from '../controllers/locationController.js';
 
-const router = express.Router();
+const locationRouter = express.Router();
 
-router
+locationRouter
   .route('/')
   .get(locationController.getLocations)
   .post(locationController.addLocation)
   .delete(locationController.deleteAllLocations);
 
-router
+locationRouter
   .route('/:id')
   .get(locationController.getLocation)
   .patch(locationController.updateLocation)
   .delete(locationController.deleteLocation);
 
 
-export default router
+export default locationRouter
