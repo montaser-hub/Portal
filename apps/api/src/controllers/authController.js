2c53219f8c0579d8c626d0a279d81c07e1de8fbe
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-10-30 22:14:17 +0300

    ..

--- apps/api/src/controllers/authController.js
+++ apps/api/src/controllers/authController.js
@@ -36,12 +36,12 @@ export const isAuth = catchAsync(async (req, res, next) => {
   //1) Get token and check of it's there
   let token;
   if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
+    req.headers?.authorization &&
+    req.headers.authorization?.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
+  } else if (req.cookies?.jwt) {
+    token = req.cookies?.jwt;
   }
 
   if (!token) {
--- apps/api/src/routes/departmentRoutes.js
+++ apps/api/src/routes/departmentRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import * as departmentController from '../controllers/departmentController.js';
-
+import * as authController from '../controllers/authController.js';
 const departmentRouter = express.Router();
-
+departmentRouter.use(authController.isAuth);
 departmentRouter
   .route('/')
   .get(departmentController.getAllDepartments)
