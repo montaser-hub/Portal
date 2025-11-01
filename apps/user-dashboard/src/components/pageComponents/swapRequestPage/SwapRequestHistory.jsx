d8d8f859def0c486ac9b9dad4978fd04031db186
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-11-01 13:26:24 +0200

    only Swap page

---
+++ apps/user-dashboard/src/components/pageComponents/swapRequestPage/SwapRequestHistory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Text from "../../common/Text";
+
+export default function SwapRequestHistory() {
+  return (
+    <div className="text-gray-600">
+      <Text as="p" content="No past swap requests found." />
+    </div>
+  );
+}
+
