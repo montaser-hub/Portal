157a653e1ea82c64c1a0ca9bf05ca9df3853465e
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-11-01 18:29:00 +0200

    pdate project files and improve backend functionality
    dd populate for Level, Department, and SubDepartment relations

--- apps/api/src/models/subdepartModel.js
+++ apps/api/src/models/subdepartModel.js
@@ -4,5 +4,13 @@ const subDepartmentSchema = new mongoose.Schema({
     departmentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
     subManagerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
+subDepartmentSchema.set('toObject', { virtuals: true });
+subDepartmentSchema.set('toJSON', { virtuals: true });
+subDepartmentSchema.virtual('department', {
+  ref: 'Department',
+  localField: 'departmentId',
+  foreignField: '_id',
+  justOne: true
+});
 const SubDepartment = mongoose.model('SubDepartment', subDepartmentSchema);
 export default SubDepartment;
