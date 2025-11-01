5f31c37697b644039c939429b7f82c0aec1bd4fc
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-11-01 13:31:30 +0200

    ..

--- apps/user-dashboard/src/router/AppRouter.jsx
+++ apps/user-dashboard/src/router/AppRouter.jsx
@@ -1,20 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
 import Navbar from "../components/layout/nav/Navbar";
-import CalendarPage from "../pages/CalendarPage";
 import SwapRequestPage from "../pages/swapRequestPage";
-import Profile from "../pages/ProfilePage";
 
 export default function AppRouter() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/swapRequests" element={<SwapRequestPage />} />
-        <Route path="/profile" element={<Profile />}  />
-
       </Routes>
     </>
   );
