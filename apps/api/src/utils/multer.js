012de5f6e1beeafb65abdc25c31dee5efa0deaee
montaser-hub <montaserismail20@gmail.com>
2025-11-11 08:32:13 +0200

    add aws s3 storage

--- apps/api/src/utils/multer.js
+++ apps/api/src/utils/multer.js
@@ -4,15 +4,15 @@ const storage = multer.memoryStorage();
 
 const fileFilter = (type = 'image') => (req, file, cb) => {
   if (type === 'image' && !file.mimetype.startsWith('image')) {
-    throw new Error('Please upload a valid image file')
+    return cb(new Error('Please upload a valid image file'), false);
   }
   cb(null, true);
 };
 
 export const uploadSingle = (fieldName, type = 'image') => {
-  return multer({ storage, fileFilter: fileFilter(type) }).single(fieldName);
+  return multer({ storage, fileFilter: fileFilter(type), limits: { fileSize: 5 * 1024 * 1024 } }).single(fieldName);
 };
 
 export const uploadMultiple = (fieldName, maxCount = 5, type = 'image') => {
-  return multer({ storage, fileFilter: fileFilter(type) }).array(fieldName, maxCount);
+  return multer({ storage, fileFilter: fileFilter(type), limits: { fileSize: 5 * 1024 * 1024 } }).array(fieldName, maxCount);
 };
