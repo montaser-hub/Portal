db8e081a0b4ca14fa2f548b478d199199feebdc8
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-11-14 15:27:13 +0200

    app.spec

--- apps/user-dashboard/src/app/app.spec.jsx
+++ apps/user-dashboard/src/app/app.spec.jsx
@@ -1,13 +1,17 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import myStore  from '../app/Redux/store';
 import App from './app';
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={myStore}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
     expect(baseElement).toBeTruthy();
-  });
+});
 });
