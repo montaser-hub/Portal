13452b69597e28279c6dea7872ea888c68804345
montaser-hub <montaserismail20@gmail.com>
2025-11-17 04:42:10 +0200

    update

--- apps/user-dashboard/src/app/app.jsx
+++ apps/user-dashboard/src/app/app.jsx
@@ -1,10 +1,6 @@
 import { Toaster } from 'react-hot-toast';
-import { useSelector } from 'react-redux';
-import useAppInit from '../hooks/useAppInit';
 import AppRouter from '../router/AppRouter';
 export function App() {
-    const user = useSelector((state) => state.user.user);
-    useAppInit(user);
   return (
     <>
     <AppRouter />
