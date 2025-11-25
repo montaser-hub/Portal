939d410ca1e2580ef2603369c727916b67ae863a
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-11-25 02:13:54 +0200

    app router after remove spinner page

--- apps/user-dashboard/src/router/AppRouter.jsx
+++ apps/user-dashboard/src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import LoginPage from '../pages/Auth/LoginPage';
 import Navbar from '../components/layout/nav/Navbar';
 import Dashboard from '../pages/Dashboard';
 import CalendarPage from '../pages/CalendarPage';
-import MySchedules from '../pages/MySchedules.jsx';
+import MySchedules from '../pages/mySchedules';
 import SwapRequestPage from '../pages/SwapRequestPage';
 import Profile from '../pages/ProfilePage';
 import NotFoundPage from '../pages/NotFoundPage';
@@ -12,7 +12,6 @@ import NotificationsPage from '../pages/NotificationPage';
 import ForgotPasswordPage from '../pages/Auth/ForgotPasswordPage';
 import ResetPasswordPage from '../pages/Auth/ResetPasswordPage';
 import ProtectedRoute from './ProtectedRoute';
-import SpinnerPage from '../pages/SpinnerPage.jsx';
 import { PublicRoute } from './ProtectedRoute.jsx';
 import Footer from '../components/layout/Footer';
 import AIAssistant from '../pages/Assistant';
@@ -36,7 +35,6 @@ export default function AppRouter() {
         <Route path="/Login" element={<LoginPage />} />
         <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
         <Route path="/ResetPassword" element={<ResetPasswordPage />} />
-        <Route path="/Spinner" element={<SpinnerPage />} />
       </Route>
 
       <Route element={<ProtectedRoute />}>
