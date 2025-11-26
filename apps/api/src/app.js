1918c5af2c9d5cb401494450960c614241a6f147
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-11-26 14:39:07 +0200

    update in cors

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -18,7 +18,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3001', 'http://localhost:4200','https://tangerine-cuchufli-e07247.netlify.app/shifts'];
+    const allowedOrigins = ['http://localhost:3001', 'http://localhost:4200','https://tangerine-cuchufli-e07247.netlify.app'];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
