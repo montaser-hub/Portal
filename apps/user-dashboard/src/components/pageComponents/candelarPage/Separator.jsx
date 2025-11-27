f0f118d0be9d48103d4ee00c3deac94c8706fc53
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-11-28 01:40:18 +0200

    ....

--- apps/user-dashboard/src/components/pageComponents/candelarPage/Separator.jsx
+++ apps/user-dashboard/src/components/pageComponents/candelarPage/Separator.jsx
@@ -1,22 +1,3 @@
-// import * as SeparatorPrimitive from "@radix-ui/react-separator";
-
-// // ---------------- Separator ----------------
-// export default function Separator({ className = "", orientation = "horizontal", decorative = true, ...props }) {
-//   return (
-//     <SeparatorPrimitive.Root
-//       data-slot="separator-root"
-//       decorative={decorative}
-//       orientation={orientation}
-//       className={
-//         (orientation === "horizontal" ? `bg-[#E5E7EB] shrink-0 h-px w-full` : `bg-[#E5E7EB] shrink-0 h-full w-px`) +
-//         className
-//       }
-//       {...props}
-//     />
-//   );
-// }
-
-// Separator.jsx
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 export default function Separator({ className = "", orientation = "horizontal", decorative = true, ...props }) {
